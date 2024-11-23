6568f22f4df09f3a25052ed2a07b9a81bc77292a
Bharath A <bharath.rmgm03@gmail.com>
2024-11-23 18:16:07 +0530

    first commint

---
+++ src/Fetch_all_the_text_links.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Fetch_all_the_text_links {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.setProperty("webdriver.chrome.driver","./softwares/chromedriver.exe");
+           WebDriver driver = new ChromeDriver();
+           driver.navigate().to("https://www.amazon.com");
+           driver.manage().window().maximize();
+            List<WebElement> links = driver.findElements(By.xpath("//a"));
+           int count = links.size();
+           for(int i=0; i<count; i++)
+        	   {
+        	 WebElement com = links.get(i);
+        	String text = com.getText();
+        	System.out.println(i+" "+text);
+        	   }
+        	   driver.quit();}
+	}
+
+
